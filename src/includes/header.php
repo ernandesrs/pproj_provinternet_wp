06f2d638d60977a7fd826cbed2ee4b683d7925aa
Ernandes R Souza <ernandesrsouza@gmail.com>
2023-10-21 08:03:00 -0400

    Title

--- src/includes/header.php
+++ src/includes/header.php
@@ -11,6 +11,8 @@ require __DIR__ . '/../bootstrap.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="<?= asset('css/style.css') ?>" rel="stylesheet">
+
+    <title>Net Fácil</title>
 </head>
 
 <body>
