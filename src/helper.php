c0b4d9f61b2ba07a3aa96ad7e652ddd4474b9b1a
Ernandes R Souza <ernandesrsouza@gmail.com>
2023-10-21 09:05:56 -0400

    Component loader

--- src/helper.php
+++ src/helper.php
@@ -21,3 +21,26 @@ function asset(string $resource)
 {
     return url() . '/assets/' . (str_starts_with($resource, '/') ? $resource : '/' . $resource);
 }
+
+/**
+ * Load component
+ *
+ * @param string $path
+ * @param array $data
+ * @return void
+ */
+function render_component(string $path, array $data = [])
+{
+    foreach ($data as $key => $value) {
+        $$key = $value;
+    }
+
+    ob_start();
+    include __DIR__ . '/components/' . $path . '.php';
+    $cmpnt = ob_get_contents();
+    ob_end_clean();
+
+    echo $cmpnt;
+
+    return;
+}
