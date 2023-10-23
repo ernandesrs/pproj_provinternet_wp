d55aa471a754b289748573d883b1ecae870726a7
Ernandes R Souza <ernandesrsouza@gmail.com>
2023-10-23 08:51:23 -0400

    Footer

--- src/includes/footer.php
+++ src/includes/footer.php
@@ -1,7 +1,49 @@
 <footer class="min-h-[365px] flex items-center text-basi-4">
     <div class="container">
-        <div class="flex flex-wrap py-10">
-            <div class="lg:basis-4/12 flex items-start">
+        <div class="flex flex-wrap justify-center py-20">
+            <div class="basis-full sm:basis-6/12 md:basis-4/12 mb-8 md:mb-0">
+                <div>
+                    <h3 class="uppercase text-base font-semibold mb-4">
+                        Menu
+                    </h3>
+                    <nav class="flex flex-col text-primary-1 ml-2">
+                        <?php
+                        foreach ($navigations as $nav):
+                            $nav = (object) $nav;
+                            ?>
+                            <a class="py-2 hover:pl-1 duration-200 hover:text-basi-6" href="<?= $nav->href ?>"
+                                title="<?= $nav->title ?? $nav->text ?>" target="<?= $nav->target ?? '_self' ?>">
+                                <?= $nav->text ?>
+                            </a>
+                            <?php
+                        endforeach;
+                        ?>
+                    </nav>
+                </div>
+            </div>
+
+            <div class="basis-full sm:basis-6/12 md:basis-3/12 mb-8 md:mb-0">
+                <div>
+                    <h3 class="uppercase text-base font-semibold mb-4">
+                        Links
+                    </h3>
+                    <nav class="flex flex-col text-primary-1 ml-2">
+                        <?php
+                        foreach ([['text' => 'Termos de privacidade', 'href' => '#'], ['text' => 'Termos de serviço', 'href' => '#']] as $nav):
+                            $nav = (object) $nav;
+                            ?>
+                            <a class="py-2 hover:pl-1 duration-200 hover:text-basi-6" href="<?= $nav->href ?>"
+                                title="<?= $nav->title ?? $nav->text ?>" target="<?= $nav->target ?? '_self' ?>">
+                                <?= $nav->text ?>
+                            </a>
+                            <?php
+                        endforeach;
+                        ?>
+                    </nav>
+                </div>
+            </div>
+
+            <div class="basis-full md:basis-5/12 flex items-start md:order-first">
                 <img src="<?= asset('img/logo-light.svg') ?>" alt="<?= CONF_NAME ?> FOOTER LOGO">
                 <div class="flex p-6 gap-4">
                     <?php
@@ -33,18 +75,10 @@
                     ?>
                 </div>
             </div>
-
-            <div class="lg:basis-4/12">
-
-            </div>
-
-            <div class="lg:basis-4/12">
-
-            </div>
         </div>
 
         <!-- copyright -->
-        <div class="text-sm py-3 cursor-default text-center">
+        <div class="text-sm py-4 cursor-default text-center">
             <a class="text-primary-1" href="<?= url() ?>" title="<?= CONF_NAME ?>">
                 <?= CONF_NAME ?>
             </a> ©
