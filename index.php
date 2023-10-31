4dc417f05472cd36e20fd92faa8a98c6cb5ccf08
Ernandes R Souza <ernandesrsouza@gmail.com>
2023-10-31 18:54:32 -0400

    Simple updates

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
 <?php
 
+$page = 'home';
+if ($p = $_GET['page'] ?? null) {
+    if (!file_exists(__DIR__ . '/src/pages/' . $p . '.php')) {
+        header('Location: ' . url());
+        exit;
+    }
+
+    $page = $p;
+}
+
 /**
  * 
  * Load header
@@ -12,15 +22,6 @@ include __DIR__ . '/src/includes/header.php';
  * Load main content
  * 
  */
-$page = 'home';
-if ($p = $_GET['page'] ?? null) {
-    if (!file_exists(__DIR__ . '/src/pages/' . $p . '.php')) {
-        header('Location: ' . url());
-        exit;
-    }
-
-    $page = $p;
-}
 
 include __DIR__ . '/src/includes/main.php';
 
